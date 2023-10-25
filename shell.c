b3cba31df456ec498f7a648bf1bd1164b7e4c9c4
Isaac <levine.i@northeastern.edu>
2023-10-25 21:55:11 +0000

    broke tokenize function into separate file

--- shell.c
+++ shell.c
@@ -1,57 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "shell.h"
-
-// Function to check if a character is a special token
-int is_special(char ch) {
-    return (ch == '(' || ch == ')' || ch == '<' || ch == '>' || ch == ';' || ch == '|');
-}
-
-Token* tokenize(const char* input) {
-// Function to tokenize the input into a list of tokens
-    Token* tokens = malloc(MAX_INPUT_LENGTH * sizeof(Token));
-    int tokenCount = 0;
-    int i = 0;
-
-    while (input[i] != '\0') {
-        if (is_special(input[i])) {
-            tokens[tokenCount].type = 'S';
-            tokens[tokenCount].value[0] = input[i];
-            tokens[tokenCount].value[1] = '\0';
-            tokenCount++;
-            i++;
-        } else if (input[i] == ' ' || input[i] == '\t' || input[i] == '\n') {
-            // Skip whitespace
-            i++;
-        } else if (input[i] == '"') {
-            i++; // Skip opening quote
-            int j = 0;
-            while (input[i] != '"' && input[i] != '\0') {
-                tokens[tokenCount].type = 'W';
-                tokens[tokenCount].value[j] = input[i];
-                j++;
-                i++;
-            }
-            tokens[tokenCount].value[j] = '\0'; // Null-terminate the token value
-            tokenCount++;
-            i++; // Skip closing quote
-        } else {
-            int j = 0;
-            while (!is_special(input[i]) && input[i] != ' ' && input[i] != '\t' && input[i] != '\n' && input[i] != '\0') {
-                tokens[tokenCount].type = 'W';
-                tokens[tokenCount].value[j] = input[i];
-                j++;
-                i++;
-            }
-            tokens[tokenCount].value[j] = '\0'; // Null-terminate the token value
-            tokenCount++;
-        }
-    }
-
-    tokens[tokenCount].value[0] = '\0'; // Null-terminate the token array
-    return tokens;
-}
+// #include "shell.h"
+#include "tokens.h"
 
 int main(int argc, char **argv) {
     char input[MAX_INPUT_LENGTH];
@@ -61,7 +12,14 @@ int main(int argc, char **argv) {
 
     while (1) {
         printf("shell $ ");
-        fgets(input, MAX_INPUT_LENGTH, stdin);
+
+        // Check if we reached the end of file (Ctrl-D)
+        if (fgets(input, MAX_INPUT_LENGTH, stdin) == NULL) {
+            if (feof(stdin)) {
+                printf("\nBye bye.\n");
+                break;
+            }
+        }
 
         if (strcmp(input, "exit\n") == 0) {
             printf("Bye bye.\n");
--- tokenize.c
+++ tokenize.c
@@ -1,56 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "shell.h" // Include the shell.h file
+#include "tokens.h"
 
-// Function to check if a character is a special token
-int is_special(char ch) {
-    return (ch == '(' || ch == ')' || ch == '<' || ch == '>' || ch == ';' || ch == '|');
-}
-
-Token* tokenize(const char* input) {
-// Function to tokenize the input into a list of tokens
-    Token* tokens = malloc(MAX_INPUT_LENGTH * sizeof(Token));
-    int tokenCount = 0;
-    int i = 0;
-
-    while (input[i] != '\0') {
-        if (is_special(input[i])) {
-            tokens[tokenCount].type = 'S';
-            tokens[tokenCount].value[0] = input[i];
-            tokens[tokenCount].value[1] = '\0';
-            tokenCount++;
-            i++;
-        } else if (input[i] == ' ' || input[i] == '\t' || input[i] == '\n') {
-            // Skip whitespace
-            i++;
-        } else if (input[i] == '"') {
-            i++; // Skip opening quote
-            int j = 0;
-            while (input[i] != '"' && input[i] != '\0') {
-                tokens[tokenCount].type = 'W';
-                tokens[tokenCount].value[j] = input[i];
-                j++;
-                i++;
-            }
-            tokens[tokenCount].value[j] = '\0'; // Null-terminate the token value
-            tokenCount++;
-            i++; // Skip closing quote
-        } else {
-            int j = 0;
-            while (!is_special(input[i]) && input[i] != ' ' && input[i] != '\t' && input[i] != '\n' && input[i] != '\0') {
-                tokens[tokenCount].type = 'W';
-                tokens[tokenCount].value[j] = input[i];
-                j++;
-                i++;
-            }
-            tokens[tokenCount].value[j] = '\0'; // Null-terminate the token value
-            tokenCount++;
-        }
-    }
-
-    tokens[tokenCount].value[0] = '\0'; // Null-terminate the token array
-    return tokens;
-}
+// A demo program
 int main(int argc, char **argv) {
     char input[MAX_INPUT_LENGTH];
     fgets(input, MAX_INPUT_LENGTH, stdin);
