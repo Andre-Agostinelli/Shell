05e2eb0c6f12ac4802faf0ce052dcd71cffc1f67
Student User <user@agostinellian-cs3650fa23-vm.khoury.northeastern.edu>
2023-10-25 14:41:34 +0000

    Added tokenize function definition into tokenize.c to fix header problem

--- tokenize.c
+++ tokenize.c
@@ -2,6 +2,55 @@
 #include <stdlib.h>
 #include "shell.h" // Include the shell.h file
 
+// Function to check if a character is a special token
+int is_special(char ch) {
+    return (ch == '(' || ch == ')' || ch == '<' || ch == '>' || ch == ';' || ch == '|');
+}
+
+Token* tokenize(const char* input) {
+// Function to tokenize the input into a list of tokens
+    Token* tokens = malloc(MAX_INPUT_LENGTH * sizeof(Token));
+    int tokenCount = 0;
+    int i = 0;
+
+    while (input[i] != '\0') {
+        if (is_special(input[i])) {
+            tokens[tokenCount].type = 'S';
+            tokens[tokenCount].value[0] = input[i];
+            tokens[tokenCount].value[1] = '\0';
+            tokenCount++;
+            i++;
+        } else if (input[i] == ' ' || input[i] == '\t' || input[i] == '\n') {
+            // Skip whitespace
+            i++;
+        } else if (input[i] == '"') {
+            i++; // Skip opening quote
+            int j = 0;
+            while (input[i] != '"' && input[i] != '\0') {
+                tokens[tokenCount].type = 'W';
+                tokens[tokenCount].value[j] = input[i];
+                j++;
+                i++;
+            }
+            tokens[tokenCount].value[j] = '\0'; // Null-terminate the token value
+            tokenCount++;
+            i++; // Skip closing quote
+        } else {
+            int j = 0;
+            while (!is_special(input[i]) && input[i] != ' ' && input[i] != '\t' && input[i] != '\n' && input[i] != '\0') {
+                tokens[tokenCount].type = 'W';
+                tokens[tokenCount].value[j] = input[i];
+                j++;
+                i++;
+            }
+            tokens[tokenCount].value[j] = '\0'; // Null-terminate the token value
+            tokenCount++;
+        }
+    }
+
+    tokens[tokenCount].value[0] = '\0'; // Null-terminate the token array
+    return tokens;
+}
 int main(int argc, char **argv) {
     char input[MAX_INPUT_LENGTH];
     fgets(input, MAX_INPUT_LENGTH, stdin);
@@ -17,3 +66,4 @@ int main(int argc, char **argv) {
     free(tokens);
     return 0;
 }
+
