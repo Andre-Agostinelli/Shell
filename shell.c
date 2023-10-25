516c38948ba9d460313e0ee87a8070a228a02bea
Isaac <levine.i@northeastern.edu>
2023-10-25 05:31:57 +0000

    stuff

--- shell.c
+++ shell.c
@@ -4,12 +4,6 @@
 
 #define MAX_INPUT_LENGTH 1024
 
-// int main(int argc, char **argv) {
-
-//   // TODO: Implement your shell's main
-//   return 0;
-// }
-
 // Define a struct to hold tokens
 typedef struct {
     char type; // 'W' for word, 'S' for special character
@@ -64,4 +58,33 @@ Token* tokenize(const char* input) {
 
     tokens[tokenCount].value[0] = '\0'; // Null-terminate the token array
     return tokens;
+}
+
+int main(int argc, char **argv) {
+    char input[MAX_INPUT_LENGTH];
+    Token* tokens;
+
+    printf("Welcome to mini-shell.\n");
+
+    while (1) {
+        printf("shell $ ");
+        fgets(input, MAX_INPUT_LENGTH, stdin);
+
+        if (strcmp(input, "exit\n") == 0) {
+            printf("Bye bye.\n");
+            break;
+        }
+
+        tokens = tokenize(input);
+
+        for (int i = 0; *tokens[i].value != '\0'; i++) {
+            printf("Token %d: %s, Type: %c\n", i, tokens[i].value, tokens[i].type);
+        }
+
+        // Now you can process and execute the command using the tokens.
+
+        free(tokens);
+    }
+
+    return 0;
 }
